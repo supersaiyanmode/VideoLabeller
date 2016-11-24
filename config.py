a37e91a6a6b83697a53351ae2cd09b5c827102d7
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-11-24 03:26:02 -0500

    Adding config file.

---
+++ config.py
@@ -0,0 +1,14 @@
+
+class BaseConfig(object):
+    tessellation_levels = 1
+
+    def __init__(self):
+        self.n_faces = 20 * (4 ** self.tessellation_levels)
+
+class CurrentConfig(BaseConfig):
+    def __init__(self):
+        super(CurrentConfig, self).__init__()
+
+_c = CurrentConfig()
+for key in dir(_c):
+    globals()[key] = getattr(_c, key)
