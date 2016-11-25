6551fe2e89a24920f57057ef99b66cb24c482dc5
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-11-24 19:51:51 -0500

    Adding more options.

--- config.py
+++ config.py
@@ -4,6 +4,15 @@ class BaseConfig(object):
     tessellation_radius = 1
     
     two_peak = True
+
+    mag_factor = 3
+    index_size = 2
+    index_sigma = 5.0
+
+    smooth_flag = False
+
+
+    
     def __init__(self):
         self.n_faces = 20 * (4 ** self.tessellation_levels)
 
