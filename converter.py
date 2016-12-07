a34ed4f46f9e536aaeb030c30ee102decf8bffde
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-12-07 06:31:49 -0500

    Removed old script.

--- converter.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-def convert(path, target):
-    capture = cv2.VideoCapture(path)
-    img = None
-    writer = cv2.VideoWriter(target, fourcc, 24, ..)
