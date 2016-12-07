0701cc4841670f9637ab294a85633616097182ce
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-12-07 06:32:44 -0500

    Main entrypoint clean up.

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import os
+import sys
 
 from data import DataSet
-import config
 from sift_classifier import train_test as train_test_sift
-from cnn_classifier import train_test as train_test_cnn
+from cnn import train_test as train_test_cnn
 
 
 def main():
-    d = DataSet("dataset", train_file="train-small-1.txt",
-                test_file="test-small-1.txt")
     d = DataSet("dataset")
     
-    if True:
-        train_test_cnn(d)
+    if sys.argv[1:] and sys.argv[1] == 'sift':
+        print train_test_sift(d)
     else:
-        train_test_sift(d)
-
-    print confusion_matrix(actual, pred)
+        print train_test_cnn(d)
 
 if __name__ == '__main__':
     main()
